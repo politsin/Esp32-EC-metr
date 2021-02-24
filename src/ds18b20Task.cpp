4c3ec36cf40ebc2fc6fa2885e01de4c68a5fd77b
Anatoliy Politsin <politsin@gmail.com>
2021-02-24 19:22:07 +0300

    tempEx

--- src/ds18b20Task.cpp
+++ src/ds18b20Task.cpp
@@ -10,21 +10,18 @@ DallasTemperature sensors(&oneWire);
 
 void ds18b20Task(void *pvParam) {
   sensors.begin();
+  sensors.setWaitForConversion(false);
   while (true) {
     sensors.requestTemperatures();
     vTaskDelay(750 / portTICK_PERIOD_MS);
     float tempEC = sensors.getTempCByIndex(0);
     if (tempEC != DEVICE_DISCONNECTED_C) {
-      if (tempEC > -60 && tempEC < 60) {
-        // char data[80];
-        // sprintf(data, "%.2f", tempC1);
-        // string metric = std::string(data);
-        // mqttMessage msg = {"tin", metric};
-        // xQueueSend(mqttQueue, &msg, portMAX_DELAY);
-        printf("tempEC = %.2f C\n", tempEC);
-      } else {
-        printf("Error: temperature out of range: %.2f C\n", tempEC);
-      }
+      char data[8];
+      sprintf(data, "%.2f", tempEC);
+      string metric = std::string(data);
+      mqttMessage msg = {"tin", metric};
+      xQueueSend(mqttQueue, &msg, portMAX_DELAY);
+      printf("tempEC = %.2f C\n", tempEC);
     } else {
       printf("Error: Could not read temperature data\n");
     }
